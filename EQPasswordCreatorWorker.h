4d32feec8301a7a60462d01f399cdbb977391216
Force-quit <miloulune1@gmail.com>
2023-05-16 21:20:12 -0400

    Splitting from Projects repo

---
+++ EQPasswordCreatorWorker.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <QObject>
+#include <QString>
+#include <QVector>
+#include <QChar>
+#include <QFile>
+
+class EQPasswordCreatorWorker : public QObject
+{
+	Q_OBJECT
+
+public:
+	EQPasswordCreatorWorker(unsigned int passwordLength);
+	~EQPasswordCreatorWorker();
+	
+	void loadAlphabet(QFile& opennedFile);
+
+public slots:
+	void generatePassword();
+	void setPasswordLength(const unsigned int passwordLength);
+
+signals:
+	void passwordGenerated(const QString password);
+
+private:
+	QVector<QChar> currentAlphabet;
+	unsigned int passwordLength;
+	unsigned int randomIndex;
+};
